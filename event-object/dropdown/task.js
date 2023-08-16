eba294bd76fa1d6c71275f5943bee38d8a0d005e
Victoria Wolf <victoriiwolf@yahoo/com>
2023-08-16 16:03:30 +0700

    homework still in process

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -2,14 +2,3 @@ const items = Array.from(document.querySelectorAll('.dropdown__item'))
 const links = Array.from(document.querySelectorAll('.dropdown__link'));
 const value = document.querySelector('dropdown__value');
 const dropdown = document.querySelector('dropdown');
-
-items.forEach(e => {
-    e.addEventListener('click', e => {
-        const menu = e.currentTarget.dataset.path;
-        dropdown.forEach(e => {
-            if (!menu.classlist.contains('dropdown__value:after')) {
-                menu.classlist.add('dropdown__list_active')
-            }
-        })
-    })
-})
