58ee73953f5bd62df97034c97e6006a2713d268e
Victoria Wolf <victoriiwolf@yahoo/com>
2023-09-07 22:41:51 +0700

    homework is in process

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,20 +3,20 @@ const tabsContents = Array.from(document.querySelectorAll('.tab__content'))
 
 tabs.addEventListener('click', function() {
     for(let tab of tabs) {
-    if (tab.contains('.tab_active')) {
-        tab.remove('.tab_active')
+    if (tab.classlist.contains('.tab_active')) {
+        tab.classlist.remove('.tab_active')
     } else {
-        tab.add('.tab_active')
+        tab.classlist.add('.tab_active')
     }
 }
 })
 
 tabsContents.addEventListener('click', function() {
     for(let tabContents of tabsContents) {
-    if(tabContents.contains('.tab__content_active')) {
-        tabContents.remove('.tab__content_active')
+    if(tabContents.classlist.contains('.tab__content_active')) {
+        tabContents.classlist.remove('.tab__content_active')
     } else {
-        tabContents.add('.tab__content_active')
+        tabContents.classlist.add('.tab__content_active')
     }
 }
 })
