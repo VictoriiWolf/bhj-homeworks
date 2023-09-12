6f94a88768676c9aca109fb372cbe2872b702544
Victoria Wolf <victoriiwolf@yahoo/com>
2023-09-12 16:38:34 +0700

    homework still is in process

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,11 +1,18 @@
-const fontSizes = document.querySelectorAll('a');
+const fontSizes = Array.from(document.querySelectorAll('a'));
+let book = document.querySelector('.book')
+let size;
 
-fontSizes.forEach((el) => {
-    el.addEventListener('click', (event) => {
-      event.preventDefault();
-      if(el.classList.contains('font-size_active')) {
-        el.classList.remove('font-size_active');
-      };
-      el.classList.add('font-size_active');
-    });
-  });
+function pushButton(event) {
+  fontSizes.forEach(el => {
+    el.classList.remove('font-size_active')
+  })
+
+  this.classList.add('font-size_active');
+  el.dataset.size // как заставить эту вещь менять размер шрифта?
+  event.preventDefault();
+}
+
+
+  fontSizes.forEach(el => {
+    el.addEventListener('click', pushButton)
+  })
