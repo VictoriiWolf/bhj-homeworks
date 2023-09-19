7977af62eda20729b3a4249b176f9dec75753eed
Victoria Wolf <victoriiwolf@yahoo/com>
2023-09-19 15:41:45 +0700

    homework is done

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,34 +1,37 @@
-const fontSizes = document.querySelectorAll('a');
-let book = document.querySelector('.book')
+const fontSizes = document.querySelectorAll('.font-size'); 
+const book = document.querySelector('.book'); 
 
-function pushButton(event) {
-  fontSizes.forEach(el => {
-    el.classList.remove('font-size_active')
-  })
+for(let i = 0; i < fontSizes.length; i++) {
+  fontSizes[i].addEventListener('click', (event) => {
+    event.preventDefault();
 
-  this.classList.add('font-size_active');
-  event.preventDefault();
+    fontSizes.forEach(el => {
+      if(el.classList.contains('font-size_active')) {
+        el.classList.remove('font-size_active');
+      }
+    });
 
-  let option = 0;
+    if(!fontSizes[i].classList.contains('font-size_active')) {
+      fontSizes[i].classList.add('font-size_active');
+    };
 
-  fontSizes.forEach((i, index) => {
-    if(i.classList.contains('font-size_active')) {
-      option = index;
-    }
-    
+    let option = 0;
+
+    fontSizes.forEach((item, index) => {
+      if(item.classList.contains('font-size_active')) {
+        option = index;
+      }
+    });
+
     if(option === 0) {
       book.classList.add('book_fs-small');
       book.classList.remove('book_fs-big');
     } else if(option === 1) {
-      book.classList.remove('book_fs-small')
-      book.classList.remove('book_fs-big')
+      book.classList.remove('book_fs-small');
+      book.classList.remove('book_fs-big');
     } else if(option === 2) {
       book.classList.add('book_fs-big');
-      book.classList.remove('book_fs-small')
-    }
+      book.classList.remove('book_fs-small');
+    };
   });
-}
-
-  fontSizes.forEach(el => {
-    el.addEventListener('click', pushButton)
-  })
+};
